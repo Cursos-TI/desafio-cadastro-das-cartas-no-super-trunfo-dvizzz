057b1d19cb8fcedddb83d6560f60a9b18e7b65af
dvizzz <davi.maia.421@gmail.com>
2025-09-02 00:19:44 +0000

    Resolvendo problemas no código

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,10 +10,31 @@ int main() {
     // Mostrando a mensagem "Jogo de Cartas - Super Trunfo" na tela
     printf("Jogo de Cartas - Super Trunfo\n");
 
-    printf("- Carta 1 -\n Digite a letra que representa o seu estado: \n");
+    printf("- Carta 1 -\n Digite a letra que representa o estado que está na sua carta: \n");
     scanf("%c", &estado_1);
 
-    printf("A letra é: %c", estado_1);
+    printf("Digite o código da carta: \n");
+    scanf("%s", codcarta_1);
+
+    printf("Digite o nome da cidade da carta: \n");
+    scanf("%s", cidade_1);
+
+    printf("Digite o número de habitantes da cidade: \n");
+    scanf("%d", &populacao_1);
+
+    printf("Digite a área(km²) da cidade: \n");
+    scanf("%f", &area_1); 
+
+    printf("Digite o PIB da cidade: \n");
+    scanf("%f", &pib_1);
+
+    printf("Digite o número de pontos turísticos que há na cidade: \n");
+    scanf("%d", &pontosturisticos_1);
+
+    printf("Carta 1: \n - Estado: %c \n - Código da carta: %s \n - Nome da Cidade: \n", estado_1, codcarta_1, cidade_1);
+    printf("População: %d - Área: %f - PIB: %f - Número de pontos turísticos: %d", pontosturisticos_1, area_1, pib_1, pontosturisticos_1);
+
+    
 
     return 0;
 }
